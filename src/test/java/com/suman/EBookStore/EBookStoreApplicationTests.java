6d241fa2fb749c0a96dc4711fb47672585652a6c
suman9roy <suman99999roy@gmail.com>
2025-08-01 02:00:47 +0530

    initial commit in dev brunch with the project structure

---
+++ src/test/java/com/suman/EBookStore/EBookStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suman.EBookStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBookStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
