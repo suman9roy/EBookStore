264a617e2c9b5633c339ebd5eb9baf91df1c7996
suman9roy <suman99999roy@gmail.com>
2025-08-01 09:54:25 +0530

    added Responseentity

--- src/main/java/com/suman/EBookStore/controller/EBookController.java
+++ src/main/java/com/suman/EBookStore/controller/EBookController.java
@@ -1,10 +1,14 @@
 package com.suman.EBookStore.controller;
 
 import com.suman.EBookStore.Entity.Books;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.awt.print.Book;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class EBookController {
@@ -18,32 +22,48 @@ public class EBookController {
     }
 
     @GetMapping("/api/home")
-    public String hello(){
-        return "Hola !! welcome to the Book Store.";
+    public ResponseEntity<String> hello(){
+        return new ResponseEntity<>("Hola !! welcome to the Book Store.", HttpStatus.OK);
     }
     @GetMapping("/api/getAllBooks")
-    public List<Books> getAllBooks(){
-    return booklist;
+    public ResponseEntity<List<Books>> getAllBooks(){
+        if(booklist.isEmpty()){
+            ResponseEntity.notFound();
+        }
+    return ResponseEntity.ok(booklist);
 
     }
 
     @PostMapping("/api/addBook")
-    public Books addBook(@RequestBody Books books){
+    public ResponseEntity<?> addBook(@RequestBody Books books){
+        if(booklist.stream().anyMatch(x -> x.getBookId() == books.getBookId()))
+        {
+            return new ResponseEntity<>("Book is already exsit with id"+books.getBookId(),HttpStatus.CONFLICT);
+        }
         booklist.add(books);
         System.out.println(booklist);
-        return books;
+        return new ResponseEntity<>(books,HttpStatus.CREATED);
     }
     @PutMapping("/api/updateBook/{id}")
-    public Books updateBook(@PathVariable int id,@RequestBody Books books){
-       Books booksNeedToUpdated= booklist.stream().filter(x->x.getBookId()==id).findFirst().get();
+    public ResponseEntity<?> updateBook(@PathVariable int id,@RequestBody Books books){
+       Optional<Books> optionalBooks= booklist.stream().filter(x->x.getBookId()==id).findFirst();
+       if(optionalBooks.isEmpty()){
+           return new ResponseEntity<>("The Book which you trying to update does not exsist with Id "+id,HttpStatus.BAD_REQUEST);
+       }
+        Books booksNeedToUpdated=optionalBooks.get();
        booksNeedToUpdated.setBookName(books.getBookName());
        booksNeedToUpdated.setPrice(books.getPrice());
-       return booksNeedToUpdated;
+       return new ResponseEntity<>(booksNeedToUpdated,HttpStatus.OK);
     }
     @DeleteMapping("/api/deleteBook")
-    public String deleteBookById(@RequestParam int id){
-        Books deleteBook=booklist.stream().filter(x->x.getBookId()==id).findFirst().get();
+    public ResponseEntity<?> deleteBookById(@RequestParam int id){
+        Optional<Books> optionalBooks=booklist.stream().filter(x->x.getBookId()==id).findFirst();
+        if(optionalBooks.isEmpty()){
+            return new ResponseEntity<>("the Book which you trying to delete does not exsist with Id "+id,
+                    HttpStatus.BAD_REQUEST);
+        }
+        Books deleteBook=optionalBooks.get();
         booklist.remove(deleteBook);
-        return "Book is deleted";
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
